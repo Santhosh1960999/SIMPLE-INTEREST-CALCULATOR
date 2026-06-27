e99328e4a136a8942abbb2e44b7a89f6d99c5a44
Santhosh1960999 <santhoshpyaram@gmail.com>
2026-06-27 18:01:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
 let m=document.getElementById('main');
 let res=document.getElementById("result");
+let select=document.getElementById("est_date")
+let est=document.getElementById('est');
+let actual=document.getElementById('actual');
+function updateDisplay() {
+    if (select.value === "Estimate") {
+        est.style.display = "flex";
+        actual.style.display = "none";
+    } else {
+        est.style.display = "none";
+        actual.style.display = "flex";
+    }
+}
+updateDisplay();
+select.addEventListener('change',updateDisplay)
 function Submit(){
+    let a=document.getElementById("a");
+    let b=document.getElementById("b");
+    let d=document.getElementById("d");
+    let x=document.getElementById("x");
+    let y=document.getElementById("y");
+    let z=document.getElementById("z");
     let amt=document.getElementById("amount").value;
     let r=document.getElementById("Interest").value;
+    
+    let t=document.getElementById("t").value;
+    let u=document.getElementById("u").value;
+    let v=document.getElementById("v").value;
     let c=document.getElementById("c");
     let d1= new Date(document.getElementById("given_date").value);
     let d2= new Date(document.getElementById("to_date").value);
-    let days=d2-d1;
     if(d1>d2) document.innerHTML="Enter Valid Date";
-    let tdays=Math.floor(days/(24*60*60*1000))
+    let years = d2.getFullYear() - d1.getFullYear();
+    let months = d2.getMonth() - d1.getMonth();
+    let days = d2.getDate() - d1.getDate();
+
+    if (days < 0) {
+        days += 30;   // Every month is treated as 30 days
+        months--;
+    }
+
+    if (months < 0) {
+        months += 12;
+        years--;
+    }
+    let tdays;
+    t=Number(t);
+    u=Number(u);
+    v=Number(v);
+    if(select.value==='Actual'){
+     tdays = years * 360 + months * 30 + days;
+    }
+    else if(select.value==='Estimate'){
+        tdays=t*360+u*30+v;
+    }
+   
+    
     let rate=document.getElementById("rupees").value;
     let inte;
     if(r==="RS"){
@@ -21,21 +68,26 @@ function Submit(){
     }
     amt=Number(amt);
     inte=Number(inte);
-    let a=document.getElementById("a");
-    let b=document.getElementById("b");
-    let d=document.getElementById("d");
-    let x=document.getElementById("x");
-    let y=document.getElementById("y");
-    let z=document.getElementById("z");
-    
-    x.innerHTML=`Given Amount = <span style="color:yellow">${amt}RS</span>`
-    y.innerHTML=`Given Date = <span style="color:yellow">${d1.toDateString()}</span>`
-    z.innerHTML=`To Date = <span style="color:yellow">${d2.toDateString()}</span>`
-    d.innerHTML=`Total Number Of Days = <span style="color:yellow">${tdays} Days</span>`
-    a.innerHTML=`Interest Amount = <span style="color:yellow">${inte}Rs</span>`
-    b.innerHTML=`Total Amount = <span style="color:yellow">${Number(amt+inte)}Rs</span>`
-   res.style.display="flex";
-   m.style.display="none";
+    if(select==='Actual'){
+        x.innerHTML=`Given Amount = <span style="color:yellow">${amt}RS</span>`
+        y.innerHTML=`Given Date = <span style="color:yellow">${d1.toDateString()}</span>`
+        z.innerHTML=`To Date = <span style="color:yellow">${d2.toDateString()}</span>`
+        d.innerHTML=`Total Number Of Days = <span style="color:yellow">${years} Years and ${months} Months and ${days} Days</span>`
+        a.innerHTML=`Interest Amount = <span style="color:yellow">${inte}Rs</span>`
+        b.innerHTML=`Total Amount = <span style="color:yellow">${Number(amt+inte)}Rs</span>`
+        res.style.display="flex";
+        m.style.display="none";
+    }
+    else{
+        x.innerHTML=`Given Amount = <span style="color:yellow">${amt}RS</span>`
+        y.innerHTML=`Years = <span style="color:yellow">${t} Years</span>`
+        z.innerHTML=`Months = <span style="color:yellow">${u} Months</span>`
+        d.innerHTML=` Days = <span style="color:yellow"> ${v} Days</span>`
+        a.innerHTML=`Interest Amount = <span style="color:yellow">${inte}Rs</span>`
+        b.innerHTML=`Total Amount = <span style="color:yellow">${Number(amt+inte)}Rs</span>`
+        res.style.display="flex";
+        m.style.display="none";
+    }
 }
 
 function Close(){
